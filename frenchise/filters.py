d8d2833ad4884800f3a1585a4e7cdcb84bb3b0a2
unknown <mauryamanish5122000@gmil.com>
2023-10-15 14:29:05 -0700

    s3 bucket migrate success

--- frenchise/filters.py
+++ frenchise/filters.py
@@ -1,7 +1,7 @@
-import django_filters
-from .models import *
+# import django_filters
+# from .models import *
 
-class frenchise_filter(django_filters.FilterSet):
-    class Meta:
-        model = ProfileFrenchise
-        fields = '__all__'
+# class frenchise_filter(django_filters.FilterSet):
+#     class Meta:
+#         model = ProfileFrenchise
+#         fields = '__all__'
